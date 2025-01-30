b026025d8d9488b2c38de48ef73b1dc9c7d17a48
Yuki <Yuki.Walsh@Outlook.com>
2025-01-30 20:33:38 +1100

    Create AnalyticEvent.cs

---
+++ AnalyticEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace StudioServer.Client;
+
+using System.Text.Json;
+
+public class AnalyticEvent
+{
+	public string? EventName { get; set; }
+	public string? EventData { get; set; }
+
+	public Task Send()
+	{
+		string json = JsonSerializer.Serialize(this);
+		return StudioServer.PostAsync("/analytics", json, "application/json");
+	}
+}
