ab8409e7136f6343cca015082857afea1429a96c
Yuki <Yuki.Walsh@Outlook.com>
2025-01-31 20:22:31 +1100

    use enums for analytics so its obvious what we are tracking

--- AnalyticEvent.cs
+++ AnalyticEvent.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 
 public class AnalyticEvent
 {
-	public string? EventName { get; set; }
+	public AnalyticEvents Event { get; set; }
 	public string? EventData { get; set; }
 
 	public Task Send()
@@ -12,4 +12,19 @@ public class AnalyticEvent
 		string json = JsonSerializer.Serialize(this);
 		return StudioServer.PostAsync("/analytics", json, "application/json");
 	}
+
+	public static void Send(AnalyticEvents evt, string? data = null)
+	{
+		AnalyticEvent eventObj = new();
+		eventObj.Event = evt;
+		eventObj.EventData = data;
+		Task.Run(eventObj.Send);
+	}
+}
+
+public enum AnalyticEvents
+{
+	None = 0,
+
+	StudioStarted,
 }
